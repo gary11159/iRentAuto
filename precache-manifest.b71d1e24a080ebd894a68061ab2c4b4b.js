a9a4c8a4e6cffd1120c8dcf3d7a905b2323a011c
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-12-31 17:34:03 +0800

    Updates

---
+++ precache-manifest.b71d1e24a080ebd894a68061ab2c4b4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddca3d7eea8294304ef7500b4efc853a",
+    "url": "/iRentAutoFrontEnd/index.html"
+  },
+  {
+    "revision": "66573ce0d12f0e325548",
+    "url": "/iRentAutoFrontEnd/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a5e08bc66d3240874340",
+    "url": "/iRentAutoFrontEnd/static/css/main.c7741b58.chunk.css"
+  },
+  {
+    "revision": "66573ce0d12f0e325548",
+    "url": "/iRentAutoFrontEnd/static/js/2.5e30ebec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/iRentAutoFrontEnd/static/js/2.5e30ebec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e08bc66d3240874340",
+    "url": "/iRentAutoFrontEnd/static/js/main.73f58b04.chunk.js"
+  },
+  {
+    "revision": "68a65f9b3ca1062c8346",
+    "url": "/iRentAutoFrontEnd/static/js/runtime-main.473a47f7.js"
+  }
+]);
